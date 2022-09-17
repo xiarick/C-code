45cbd08e2bbb221dd12cee823399b83f1c0c187e
xiarick <2281468137@qq.com>
2022-09-17 22:02:09 +0800

    three ways to accomplish this work

--- if.c
+++ if.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	//第一种
+	//第一种方式
 	/*int age = 18;
 	if (age < 18)
 		printf("未成年\n");
 	else
 		printf("成年\n");
 	return 0;*/
-	int age = 48;
+	//第二种方式
+	/*int age = 8;
 	if (age < 18)
 		printf("未成年\n");
 	else if (age < 30)
@@ -17,7 +18,20 @@ int main(void)
 		printf("中年\n");
 	else
 		printf("老年\n");
+	return 0;*/
+	//第三种方式
+	int age = 104;
+	if (age < 18)
+		printf("未成年\n");
+	else if (age >= 18 && age < 30)
+		printf("青年\n");
+	else if (age >= 30 && age < 50)
+		printf("中年\n");
+	else if (age >= 50 && age < 90)
+		printf("老年\n");
+	else printf("长寿\n");
 	return 0;
+	
 
 
 
